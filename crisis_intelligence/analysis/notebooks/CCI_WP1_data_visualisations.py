53e0ea3177ec471865a39faebcf2a74a0d7fb2eb
nasrahussein1 <nasra.hussein@gmail.com>
2021-07-02 15:45:33 +0100

    update to charts

--- crisis_intelligence/analysis/notebooks/CCI_WP1_data_visualisations.py
+++ crisis_intelligence/analysis/notebooks/CCI_WP1_data_visualisations.py
@@ -17,9 +17,6 @@
 # %% [markdown]
 # # Data visualisations for WP1 report
 
-# %%
-# !pip install plotly
-
 # %% [markdown]
 # ### Import libraries
 
@@ -48,7 +45,13 @@ pal2 = sns.cubehelix_palette(as_cmap=True)
 
 
 def strip_white_space(df):
+    """strip white spaces from records
+    args:
+        df: dataframe
 
+    Returns:
+        df with white spaces stripped
+    """
     cols = df.select_dtypes(object).columns
     df[cols] = df[cols].apply(lambda x: x.str.strip())
     return df
@@ -58,7 +61,17 @@ def strip_white_space(df):
 
 
 def pie(x, ax=None, labels=None, title=None, **plt_kwargs):
+    """strip white spaces from records
+    args:
+        x: data
+        ax: axis
+        labels: pie chart labels
+        title: chart title
+        **plt_kwargs: keyword args
 
+    Returns:
+        pie charts
+    """
     ax = ax
     plt.pie(x=x, autopct="%.1f%%", labels=labels, **plt_kwargs)
     plt.title(title, fontsize=14)
@@ -68,9 +81,19 @@ def pie(x, ax=None, labels=None, title=None, **plt_kwargs):
 # plot sunburst chart
 
 
-def sunburst_chart(df, path=None, values=None, **kwargs):
+def sunburst_chart(df, path=None, values=None, **plt_kwargs):
+    """strip white spaces from records
+    args:
+        df: dataframe
+        path: sunburst path
+        values: chart values
+        title: chart title
+        **plt_kwargs: keyword args
 
-    fig = px.sunburst(df, path=path, values=values, **kwargs)
+    Returns:
+        interative sunburst chart
+    """
+    fig = px.sunburst(df, path=path, values=values, **plt_kwargs)
     fig.update_traces(textinfo="label+percent entry")
     return fig
 
@@ -78,12 +101,46 @@ def sunburst_chart(df, path=None, values=None, **kwargs):
 # plot bar chart
 
 
-def bar_chart(df, col, **kwargs):
+def bar_chart(df, col=None, **plt_kwargs):
+    """plot bar chart
+    args:
+        df: dataframe
+        col: columns to order chart
+        values: chart values
+        title: chart title
+        **plt_kwargs: keyword args
 
-    ax = df.sort_values(col, ascending=False).plot(
-        kind="bar", figsize=[10, 10], **kwargs
-    )
+    Returns:
+        bar chart
+    """
+
+    try:
+        df = df.sort_values(col, ascending=False)
+    except:
+        pass
+
+    ax = df.plot(kind="bar", figsize=[10, 10], **plt_kwargs)
+
     return ax
+
+
+# split solution readiness level 9 into "deployed in 1 market" and "deployed in multiple markets"
+
+
+def split_SR9(project, deployment_level):
+    """for projects with SR9, assign a new development level that splits SR9 into two
+    args:
+        project: case study project
+        deployment_level: set development level that splits SR9
+
+    Returns:
+        new development level
+    """
+
+    project_index = case_studies[case_studies["Project"] == project].index[0]
+    case_studies.iloc[
+        project_index, case_studies.columns.get_loc("readiness_categories")
+    ] = deployment_level
 
 
 # %% [markdown]
@@ -94,12 +151,22 @@ DATA_PATH = "../../../../crisis_intelligence"
 
 # %%
 case_studies = pd.read_csv(f"{DATA_PATH}/inputs/data/wp1_data_for_visualisation.csv")
+case_study_table = pd.read_csv(f"{DATA_PATH}/inputs/data/case_study_table.csv")
+CCI_case_studies = pd.read_csv(
+    f"{DATA_PATH}/inputs/data/CCI_case_studies_innovation_stage.csv"
+)
 
 # %%
 # strip white spaces from dataset and remove new lines
 
 case_studies = strip_white_space(case_studies)
 case_studies.replace(r"\s+|\\n", " ", regex=True, inplace=True)
+
+case_study_table = strip_white_space(case_study_table)
+case_study_table.replace(r"\s+|\\n", " ", regex=True, inplace=True)
+
+CCI_case_studies = strip_white_space(CCI_case_studies)
+CCI_case_studies.replace(r"\s+|\\n", " ", regex=True, inplace=True)
 
 # %%
 # count number of projects by humanitarian challenge
@@ -137,7 +204,11 @@ pie(
     title=title,
     colors=pal,
 )
+
+plt.savefig(f"{DATA_PATH}/outputs/figures/pie_chart_hum_crisis_split.png")
+
 plt.show()
+
 
 # %%
 # plot pie chart showing the split of case studies by high level crises
@@ -152,6 +223,9 @@ pie(
     title=title,
     colors=pal,
 )
+
+plt.savefig(f"{DATA_PATH}/outputs/figures/pie_chart_hum_crisis_split_v2.png")
+
 plt.show()
 
 # %% [markdown]
@@ -205,15 +279,18 @@ crisis_groupby = crisis_groupby.replace("", np.nan)
 # %%
 # plot sunburst chart
 
-sunburst_chart(
+hum_crisis_split = sunburst_chart(
     crisis_groupby,
     path=["high_level_crisis_types", "type_of_humanitarian_challenge"],
     values="Project",
     color="Project",
-    color_continuous_scale="Magenta",
+    color_continuous_scale=pal,
     width=800,
     height=800,
 )
+
+hum_crisis_split.show()
+
 
 # %% [markdown]
 # ### Classification vs regression vs other broken down by supervised, unsupervised, weakly supervised and semi-supervised
@@ -279,6 +356,7 @@ method_vs_class_vs_reg_pivot = (
 
 ax = bar_chart(method_vs_class_vs_reg_pivot, "supervised", stacked=True, color=pal)
 ax.set(xlabel="Prediction Type", ylabel="Percentage")
+plt.savefig(f"{DATA_PATH}/outputs/figures/bar_chart_methods_split.png")
 plt.show()
 
 # %% [markdown]
@@ -314,8 +392,9 @@ methods_vs_techniques_heatmap = case_studies_methods_vs_techniques.pivot_table(
 # plot method vs techniques heatmeap
 
 ax = plt.subplots(figsize=[10, 10]), sns.heatmap(
-    methods_vs_techniques_heatmap, annot=True, cmap=pal2
+    methods_vs_techniques_heatmap, annot=True, cmap=pal
 )
+plt.savefig(f"{DATA_PATH}/outputs/figures/method_vs_techniques_heatmap.png")
 plt.show()
 
 # %% [markdown]
@@ -330,23 +409,128 @@ case_studies["solution_readiness_num_score"] = pd.to_numeric(
 )
 
 # %%
-solution_readiness_dist = (
-    case_studies.groupby("solution_readiness")["Project"].count().reset_index()
+# consolidate solution readiness levels
+
+# map readiness levels 3 and 4 to proof of concept
+
+case_studies.loc[
+    (case_studies["solution_readiness_num_score"] == 3)
+    | (case_studies["solution_readiness_num_score"] == 4),
+    "readiness_categories",
+] = "SRL 3/4: Concept/Idea"
+
+# map readiness levels 5 and 6 to Prototype
+
+case_studies.loc[
+    (case_studies["solution_readiness_num_score"] == 5)
+    | (case_studies["solution_readiness_num_score"] == 6),
+    "readiness_categories",
+] = "SRL 5/6: Prototype"
+
+# map readiness level 7 pilot
+
+case_studies.loc[
+    (case_studies["solution_readiness_num_score"] == 7), "readiness_categories"
+] = "SRL 7: Pilot"
+
+# split solution readiness level 9 into "deployed in 1 market" and "deployed in multiple markets"
+
+split_SR9("Sentry Syria", "SRL 9: Operational in one market")
+split_SR9("Dataminr for humanitarian response", "SRL 9: Scaling to other markets")
+split_SR9("RapiD (MapWithAI)", "SRL 9: Scaling to other markets")
+split_SR9(
+    "Common Social Accountability Platform (CSAP)", "SRL 9: Operational in one market"
 )
-solution_readiness_dist["Project"] = (
-    solution_readiness_dist["Project"] / len(case_studies)
-) * 100
+split_SR9("AIME", "SRL 9: Scaling to other markets")
+split_SR9("Companion Modelling", "SRL 9: Scaling to other markets")
+split_SR9("Early Warning Project", "SRL 9: Scaling to other markets")
+split_SR9("Internal-displacement", "SRL 9: Scaling to other markets")
+split_SR9("Hunger map live", "SRL 9: Scaling to other markets")
+split_SR9("InaSAFE - Flood impact model", "SRL 9: Operational in one market")
+split_SR9("ViEWS: Violence Early-Warning System", "SRL 9: Scaling to other markets")
 
 # %%
-ax = solution_readiness_dist.plot(
-    kind="bar", x="solution_readiness", y="Project", figsize=[8, 8], color="purple"
+# append CCI case studies not part of the 33 PA case study analysis
+
+case_studies_CCI_vs_PA = pd.merge(case_studies, CCI_case_studies, how="outer")
+
+# %%
+# merge case study table
+
+case_studies_CCI_vs_PA = pd.merge(
+    case_studies_CCI_vs_PA,
+    case_study_table,
+    how="left",
+    left_on="Project",
+    right_on="Title",
 )
-ax.set(xlabel="Solution Readiness", ylabel="Percentage")
-ax.get_legend().remove()
+
+# %%
+# create field for PA_vs_CCI
+
+case_studies_CCI_vs_PA["PA_vs_CCI"] = np.where(
+    case_studies_CCI_vs_PA["CCI Case Study"] == "N", "Predictive Analytics only", "CCI"
+)
+
+# %%
+CCI_vs_PA = (
+    case_studies_CCI_vs_PA.groupby(["PA_vs_CCI", "readiness_categories"])[["Project"]]
+    .count()
+    .reset_index()
+)
+
+# %%
+CCI_vs_PA_pivot = CCI_vs_PA.pivot(
+    columns="PA_vs_CCI", index="readiness_categories", values="Project"
+).fillna(0)
+
+# %%
+# sort values based on maturity level
+
+CCI_vs_PA_pivot.loc[
+    CCI_vs_PA_pivot.index == "SRL 9: Scaling to other markets",
+    "readiness_categories_num",
+] = 5
+
+CCI_vs_PA_pivot.loc[
+    CCI_vs_PA_pivot.index == "SRL 9: Operational in one market",
+    "readiness_categories_num",
+] = 4
+
+CCI_vs_PA_pivot.loc[
+    CCI_vs_PA_pivot.index == "SRL 7: Pilot", "readiness_categories_num"
+] = 3
+
+CCI_vs_PA_pivot.loc[
+    CCI_vs_PA_pivot.index == "SRL 5/6: Prototype", "readiness_categories_num"
+] = 2
+
+CCI_vs_PA_pivot.loc[
+    CCI_vs_PA_pivot.index == "SRL 3/4: Concept/Idea", "readiness_categories_num"
+] = 1
+
+CCI_vs_PA_pivot.sort_values("readiness_categories_num", inplace=True)
+CCI_vs_PA_pivot.drop("readiness_categories_num", axis=1, inplace=True)
+
+# %%
+# plot bar chart showing the split of CCI vs PA by readiness categories
+
+color_map = ["#7F00FF", "#00aaba"]
+
+ax = bar_chart(CCI_vs_PA_pivot, stacked=True, color=color_map)
+ax.set(xlabel="Stage of innovation", ylabel="Number of case studies")
+plt.savefig(f"{DATA_PATH}/outputs/figures/bar_chart_split_readiness_categories.png")
 plt.show()
 
 # %% [markdown]
 # ### Create chart plotting the correlation between solution readiness and integration in humanitarian systems
+
+# %%
+# calculate number of case studies integrated into humanitarian workflows
+
+hum_workflows = case_studies_CCI_vs_PA[
+    ["Project", "PA_vs_CCI", "integrated_into_humanitarian_workflows_and_systems"]
+]
 
 # %%
 # solution readiness vs integration in humanitarian systems heatmap
@@ -371,6 +555,7 @@ ax = plt.subplots(figsize=[10, 10]), sns.heatmap(
     xlabel="Integrated into humanitarian workflows and systems",
     ylabel="Solution Readiness",
 )
+plt.savefig(f"{DATA_PATH}/outputs/figures/SR_vs_integration_heatmap.png")
 plt.show()
 
 # %% [markdown]
@@ -409,6 +594,7 @@ ax = algorithms_groupby.plot(
 )
 ax.get_legend().remove()
 ax.set(xlabel="Algorithm", ylabel="Percentage")
+plt.savefig(f"{DATA_PATH}/outputs/figures/algo_dist.png")
 plt.show()
 
 # %%
@@ -423,7 +609,7 @@ algorithms_methods_groupby = (
 # %%
 # plot sunburst chart of all algorithms
 
-fig = sunburst_chart(
+algo_sunburst = sunburst_chart(
     algorithms_methods_groupby,
     path=["AI", "method", "techniques", "algorithms"],
     color="method",
@@ -431,8 +617,16 @@ fig = sunburst_chart(
     height=1300,
 )
 
-fig.update_layout(uniformtext=dict(minsize=13, mode="show"))
-fig.update_traces(textinfo="label")
+algo_sunburst.update_layout(uniformtext=dict(minsize=13, mode="show"))
+algo_sunburst.update_traces(textinfo="label")
 
+
+# %%
+
+# %%
+
+# %%
+
+# %%
 
 # %%
